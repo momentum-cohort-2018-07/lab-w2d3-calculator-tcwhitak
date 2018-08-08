f65d03d13f434ac42e4796afb9f68ab030706597
tcwhitak <t.chad.whitaker@gmail.com>
2018-08-08 16:56:23 -0400

    created loop function for number inputs

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
 
-document.querySelector(".one").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 1
-})
+var numberButtons = document.querySelectorAll('.numberButtons')
+//looping add event listener for each number button
+for (var button of numberButtons){
+    //add event listener for individual button
+    button.addEventListener('click', function(e){
+        //define button as the element that triggered the event
+        var button = e.srcElement
+        //push button text to display (calcWindow)
+        document.querySelector(".calcWindow").innerText += button.innerText
+}
+    )
+}
 
-document.querySelector(".two").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 2
-})
 
-document.querySelector(".three").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 3
-})
+// document.querySelector(".one").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 1
+// })
 
-document.querySelector(".four").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 4
-})
+// document.querySelector(".two").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 2
+// })
 
-document.querySelector(".five").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 5
-})
+// document.querySelector(".three").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 3
+// })
 
-document.querySelector(".six").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 6
-})
+// document.querySelector(".four").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 4
+// })
 
-document.querySelector(".seven").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 7
-})
+// document.querySelector(".five").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 5
+// })
 
-document.querySelector(".eight").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 8
-})
+// document.querySelector(".six").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 6
+// })
 
-document.querySelector(".nine").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 9
-})
+// document.querySelector(".seven").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 7
+// })
 
-document.querySelector(".zero").addEventListener('click', function(){
-    document.querySelector(".calcWindow").innerText += 0
-})
+// document.querySelector(".eight").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 8
+// })
+
+// document.querySelector(".nine").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 9
+// })
+
+// document.querySelector(".zero").addEventListener('click', function(){
+//     document.querySelector(".calcWindow").innerText += 0
+// })
 
 document.querySelector(".clearButton").addEventListener('click', function(){
     document.querySelector(".calcWindow").innerText = ""
